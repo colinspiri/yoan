ec34c7101445e91187ce8cd8cf1bcf812a0d450c
Colin Spiridonov <colinspiri@gmail.com>
2022-04-06 14:12:50 -0700

    added running

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,27 +15,33 @@ public class PlayerController : MonoBehaviour
     private float gravity = -13.0f;
     private float moveSmoothTime = 0.3f;
     private float mouseSmoothTime = 0.03f;
+    
     // public constants
-    [Header("Movement")] 
+    [Header("Camera")] 
     public float mouseSensitivity = 3.5f;
     public float mousePitchClamp = 90f;
-    public float walkSpeed;
-    public float bobAmount;
+    public float bobFrequency;
     public float bobMagnitude;
-    // sounds
-    [Header("Sounds")] 
+    [Header("Movement")] 
+    public float crouchSpeed;
+    public float walkSpeed;
+    public float runSpeed;
+    [Header("Sounds")]
     public float walkLoudness;
 
     // private state
+    // camera
     private float originalCameraY;
     private float cameraPitch;
-    private Vector3 velocity;
-    private float velocityY;
     private float cycle;
-    Vector2 currentDir;
-    Vector2 currentDirVelocity;
-    Vector2 currentMouseDelta;
-    Vector2 currentMouseDeltaVelocity;
+    // movement
+    private Vector3 velocity;
+    private Vector2 currentDir;
+    private Vector2 currentDirVelocity;
+    private Vector2 currentMouseDelta;
+    private Vector2 currentMouseDeltaVelocity;
+    private enum MoveState { Walking, Running, Crouching };
+    private MoveState moveState = MoveState.Walking;
 
     private void Awake()
     {
@@ -56,7 +62,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void UpdateMouseLook() {
+    private void UpdateMouseLook() {
         Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
         currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
@@ -68,26 +74,38 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
         
         // Bob head
-        cycle += velocity.magnitude * bobAmount * Time.deltaTime;
+        cycle += velocity.magnitude * bobFrequency * Time.deltaTime;
         cycle %= 2 * Mathf.PI;
-        if (currentDirVelocity.magnitude == 0) cycle = 0;
-        Debug.Log("cycle: " + cycle);
+        if (currentDirVelocity.magnitude == 0 || moveState == MoveState.Crouching) cycle = 0;
         
         playerCamera.position = new Vector3(playerCamera.position.x, originalCameraY + Mathf.Sin(cycle) * bobMagnitude, playerCamera.position.z);
     }
 
-    void UpdateMovement() {
+    private void UpdateMovement() {
+        // get input for direction
         Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         targetDir.Normalize();
-
         currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
+        // get input for movestate
+        if (Input.GetKey(KeyCode.LeftControl)) moveState = MoveState.Crouching;
+        else if (Input.GetKey(KeyCode.LeftShift)) moveState = MoveState.Running;
+        else moveState = MoveState.Walking;
+        Debug.Log(moveState);
 
-        if(controller.isGrounded) velocityY = 0.0f;
+        // gravity
+        if(controller.isGrounded) velocity.y = 0.0f;
+        velocity.y += gravity * Time.deltaTime;
+        
+        // get speed based on movestate
+        float speed = moveState switch {
+            MoveState.Crouching => crouchSpeed,
+            MoveState.Walking => walkSpeed,
+            MoveState.Running => runSpeed,
+            _ => 0
+        };
 
-        velocityY += gravity * Time.deltaTime;
-		
-        velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * walkSpeed + Vector3.up * velocityY;
-
+        // calculate velocity
+        velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * speed + Vector3.up * velocity.y;
         controller.Move(velocity * Time.deltaTime);
     }
 
