2c88a01f97140898136d981e95e833c9c96bbf73
Colin Spiridonov <colinspiri@gmail.com>
2022-09-20 16:36:02 -0700

    added Behavior Designer

---
+++ Assets/Behavior Designer/Editor/BehaviorTreeInspector.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+using BehaviorDesigner.Editor;
+
+namespace BehaviorDesigner.Editor
+{
+    [CustomEditor(typeof(Runtime.BehaviorTree))]
+    public class BehaviorTreeInspector : BehaviorInspector
+    {
+        // intentionally left blank
+    }
+}
